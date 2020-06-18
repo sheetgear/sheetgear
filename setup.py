fd5d8f30a61b80370e705a55f8615eddfedcc73f
pnhung177 <pnhung177@gmail.com>
2020-06-18 20:04:20 +0700

    The first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+  name = 'sheetgear',
+  version = '0.0.1',
+  description = '',
+  author = 'sheetgear',
+  author_email = 'sheetgear@gmail.com',
+  license = 'GPL-3.0',
+  url = 'https://github.com/sheetgear/sheetgear',
+  download_url = 'https://github.com/sheetgear/sheetgear/downloads',
+  keywords = ['sheetgear'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
